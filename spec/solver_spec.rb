db2de6d26af280ea5b1364d615644d2059a4302f
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-04-14 18:13:26 +0530

    Fix linters

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,9 +3,9 @@
 require './solver'
 
 describe Solver do
-	before :each do
-		@solver = Solver.new
-	end
+  before :each do
+    @solver = Solver.new
+  end
   context 'factorial method returns correct result' do
     it 'Should calculate the factorial of a given number' do
       expect(@solver.factorial(5)).to eq 120
@@ -13,11 +13,10 @@ describe Solver do
       expect(@solver.factorial(1)).to eq 1
     end
   end
-	context 'Reverse method return reverse of a string' do
-		it 'Should reverse the given string' do
-			expect(@solver.reverse('hello')) .to eq 'olleh'
-			expect(@solver.reverse('Wrold')).to be_a_kind_of(String)
-
-		end
-	end
+  context 'Reverse method return reverse of a string' do
+    it 'Should reverse the given string' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+      expect(@solver.reverse('Wrold')).to be_a_kind_of(String)
+    end
+  end
 end
