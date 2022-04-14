03907d7a0ac473f893ae179782f81ab13bd47c78
Fatima Ezzahra <felemenoun@gmail.com>
2022-04-14 14:13:24 +0100

    Create fizzbuzz method based on test

--- solver.rb
+++ solver.rb
@@ -14,6 +14,18 @@ class Solver
   def reverse(string)
     string.reverse if string.is_a? String
   end
+
+  def fizzbuzz(num)
+    if (num % 5).zero? && (num % 3).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
+    end
+  end
 end
 
 # rubocop:enable Style/Documentation
