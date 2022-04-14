e6cae5b0f6890a651fa8cda22ec8e2ac942fbb70
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-04-14 18:12:49 +0530

    Test for reverse

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,15 +3,21 @@
 require './solver'
 
 describe Solver do
+	before :each do
+		@solver = Solver.new
+	end
   context 'factorial method returns correct result' do
-    before :each do
-      @solver = Solver.new
-    end
-
     it 'Should calculate the factorial of a given number' do
       expect(@solver.factorial(5)).to eq 120
       expect(@solver.factorial(0)).to eq 1
       expect(@solver.factorial(1)).to eq 1
     end
   end
+	context 'Reverse method return reverse of a string' do
+		it 'Should reverse the given string' do
+			expect(@solver.reverse('hello')) .to eq 'olleh'
+			expect(@solver.reverse('Wrold')).to be_a_kind_of(String)
+
+		end
+	end
 end
