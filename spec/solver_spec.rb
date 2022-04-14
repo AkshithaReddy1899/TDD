03fb436a73964d4ace0eaab9d99c19bc2e7c010c
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-04-14 17:49:18 +0530

    Test for factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,17 @@
+require './solver'
+
+describe Solver do
+	context 'factorial method returns correct result'
+	do
+		before :each do
+			@solver = Solver.new
+		end
+
+		it 'Should calculate the factorial of a given number'
+		do
+			expect(@solver.factorial(5)) .to eq 120
+			expect(@solver.factorial(0)) .to eq 1
+			expect(@solver.factorial(1)) .to eq 1
+		end
+	end
+end
